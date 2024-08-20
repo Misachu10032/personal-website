d721ff5a8d696aa72a2c9fd74d537140ba1cbc70
Misachu10032 <johnzhou.engineer@gmail.com>
2024-08-20 16:58:19 -0600

    build set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,20 @@ module.exports = {
     'molindo/react',
     'molindo/tailwind',
     'plugin:@next/next/recommended',
+    'prettier', // Ensure compatibility with Prettier formatting
   ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off', // Disable no-unused-vars rule for TypeScript
+    'react/function-component-definition': 'off',
+    '@next/next/no-img-element': 'off',
+    'func-style': 'off',
+    'react/button-has-type': 'off',
+    'import/order': 'off', // Disable import order rule
+
+    'tailwindcss/classnames-order': 'off', // Disable tailwind classnames order rule
+    'react/jsx-sort-props': 'off', // Disable prop sorting rule
+    'jsx-a11y/anchor-is-valid': 'off', // Disable a11y rule for anchor validity
+    'jsx-a11y/click-events-have-key-events': 'off', // Disable a11y click event key event rule
+    'jsx-a11y/no-static-element-interactions': 'off', // Disable a11y rule for static element interactions
+  },
 };
