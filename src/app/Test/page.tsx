8c925de6409f8f2c600647f3c7adcb2b1b51620b
Misachu10032 <johnzhou.engineer@gmail.com>
2024-08-20 23:35:00 -0600

    test

---
+++ src/app/Test/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React  from 'react';
+
+const ImageGallery: React.FC = () => {
+
+
+
+
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center mb-6">TEst</h1>
+
+
+    </div>
+  );
+};
+
+export default ImageGallery;
