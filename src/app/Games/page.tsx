491050816f44b22721e1edbed0c23a5dcc801da6
Misachu10032 <johnzhou.engineer@gmail.com>
2024-10-11 19:37:31 -0600

    game cover

--- src/app/Games/page.tsx
+++ src/app/Games/page.tsx
@@ -4,10 +4,18 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const games = [
-  { name: 'Snake', href: '/Games/snake', image: '/images/snake-icon.png' },
-  { name: 'Jumping Kirby', href: '/Games/jumping-kirby', image: '/images/jumping-kirby-icon.png' },
-  { name: 'Tetris', href: '/Games/tetris', image: '/images/tetris-icon.png' },
-  { name: 'Touhou', href: '/Games/touhou', image: '/images/touhou-icon.png' },
+  { name: 'Snake', href: '/Games/snake', image: '/Games/Home/SnakeGame.png' },
+  {
+    name: 'Jumping Kirby',
+    href: '/Games/jumping-kirby',
+    image: '/Games/Home/JumpingKirbyGame.png',
+  },
+  {
+    name: 'Tetris',
+    href: '/Games/tetris',
+    image: '/Games/Home/TetrisGame.png',
+  },
+
 ];
 
 const GamesPage: React.FC = () => {
@@ -18,13 +26,16 @@ const GamesPage: React.FC = () => {
         {games.map((game) => (
           <Link href={game.href} key={game.name}>
             <div className="bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">
-              <Image
-                src={game.image}
-                alt={`${game.name} icon`}
-                width={300}
-                height={300}
-                className="w-full h-full object-cover"
-              />
+              <div className="w-[300px] h-[300px]">
+                <Image
+                  src={game.image}
+                  alt={`${game.name} icon`}
+                  width={300}
+                  height={300}
+                  className="object-contain w-full h-full"
+
+                />
+              </div>
               <div className="p-4 text-center">
                 <h2 className="text-xl font-semibold">{game.name}</h2>
               </div>
